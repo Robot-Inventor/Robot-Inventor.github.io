e8db6669af215161e9db482836b6363c2ce1af9e
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-07-24 20:21:12 +0900

    Update basic.js

--- js/basic.js
+++ js/basic.js
@@ -1,5 +1,6 @@
 jQuery(window).on('load', function() {
     jQuery('#loader-bg').hide();
+    jQuery('header').load('/ORIZIN_Agent/header_menu.html');
 });
 $( function() {
     $( 'img.lazy' ).lazyload( {
