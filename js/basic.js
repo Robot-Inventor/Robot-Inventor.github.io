deaf311bd18b1a1ef57de1967fddb926cb6d4dd6
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-07-24 21:41:39 +0900

    Update basic.js

--- js/basic.js
+++ js/basic.js
@@ -1,7 +1,5 @@
-document.open();
-document.write("<script type='text/javascript' src='https://code.jquery.com/jquery-3.2.1.min.js'></script>");
-document.write("<script type='text/javascript' src='/ORIZIN_Agent/js/jquery.lazyload.min.js'></script>");
-document.close();
+document.write('<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>');
+document.write("<script src='/ORIZIN_Agent/js/jquery.lazyload.min.js'></script>");
 jQuery(window).on('load', function() {
     jQuery('#loader-bg').hide();
 });
