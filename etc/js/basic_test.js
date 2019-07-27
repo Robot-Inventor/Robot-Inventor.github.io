e2bbe178b3b6a20ab199e0997ba8fa24687d9f47
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-07-27 17:59:32 +0900

    Delete basic_test.js

--- etc/js/basic_test.js
+++
@@ -1,7 +0,0 @@
-$( function() {
-    jQuery('header').load('/ORIZIN_Agent/etc/html/header_menu.min.html');
-    jQuery('footer').load('/ORIZIN_Agent/etc/html/footer.min.html');
-    echo.init ({
-        throttle: '0'
-    });
-});
