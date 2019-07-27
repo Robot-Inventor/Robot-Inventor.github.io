d71c89a2a975f9695c9dd5dd4a2bf1dd8fad081d
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-07-27 18:02:48 +0900

    Update basic.js

--- etc/js/basic.js
+++ etc/js/basic.js
@@ -1,13 +1,7 @@
-jQuery(window).on('load', function() {
-    jQuery('#loader-bg').hide();
-});
-$( function() {
-    jQuery('header').load('/ORIZIN_Agent/header_menu.html');
-    jQuery('footer').load('/ORIZIN_Agent/etc/html/footer.html');
-    $( 'img.lazy' ).lazyload( {
-        effect: 'fadeIn',
-	effect_speed: 3000,
-	skip_invisible: true,
-	placeholder: '/ORIZIN_Agent/icon/805.png',
+$(function(){
+	jQuery("header").load("/ORIZIN_Agent/etc/html/header_menu.min.html"),
+    jQuery("footer").load("/ORIZIN_Agent/etc/html/footer.min.html"),
+    echo.init({
+        throttle:"0"
     });
 });
