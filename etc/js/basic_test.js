6fdc8a6d5d5b996929f8dea6203f639ccb8ba2c9
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-07-27 16:25:40 +0900

    Update basic_test.js

--- etc/js/basic_test.js
+++ etc/js/basic_test.js
@@ -1,6 +1,6 @@
 $( function() {
-    jQuery('header').load('/ORIZIN_Agent/etc/html/header_menu.html');
-    jQuery('footer').load('/ORIZIN_Agent/etc/html/footer.html');
+    jQuery('header').load('/ORIZIN_Agent/etc/html/header_menu.min.html');
+    jQuery('footer').load('/ORIZIN_Agent/etc/html/footer.min.html');
     echo.init ({
         throttle: '0'
     });
