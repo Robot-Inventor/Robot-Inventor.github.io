1f85cac69764f2e459c7160952c65b6ae23f38a5
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-10-31 19:58:13 +0900

    Update basic.js

--- etc/js/basic.js
+++ etc/js/basic.js
@@ -4,9 +4,9 @@ $(function(){
     echo.init({
         throttle:"0"
     });
-    var darkmode = document.createElement("script");
-    darkmode.type = "text/javascript";
-    darkmode.src = "https://cdn.jsdelivr.net/npm/darkmode-js@1.5.1/lib/darkmode-js.min.js";
-    document.body.appendChild(darkmode);
-    new Darkmode().showWidget();
+    var darkmode = document.createElement("script"),
+    darkmode.type = "text/javascript",
+    darkmode.src = "https://cdn.jsdelivr.net/npm/darkmode-js@1.5.1/lib/darkmode-js.min.js",
+    document.body.appendChild(darkmode),
+    new Darkmode().showWidget()
 });
