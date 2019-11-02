670ebfe43bd0c6445a15a5f1eb02616f0009c3e1
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-11-02 23:13:12 +0900

    Update darkthemetest.js

--- etc/js/darkthemetest.js
+++ etc/js/darkthemetest.js
@@ -1,6 +1,6 @@
-var darkmode = document.createElement('script');
-darkmode.src = 'https://cdn.jsdelivr.net/npm/darkmode-js@1.5.1/lib/darkmode-js.min.js';
-document.body.appendChild(darkmode);
+var darkmodelib = document.createElement('script');
+darkmodelib.src = 'https://cdn.jsdelivr.net/npm/darkmode-js@1.5.1/lib/darkmode-js.min.js';
+documentlib.body.appendChild(darkmode);
 
 $(function(){
     jQuery("header").load("/etc/html/header_menu.min.html");
@@ -8,5 +8,9 @@ $(function(){
     echo.init({
         throttle:"0"
     });
-    new Darkmode().showWidget();
+    var options = {
+        label: '🌓'
+    }
+    const darkmode = new Darkmode(options);
+    window.setTimeout(darkmode.showWidget, 0);
 });
