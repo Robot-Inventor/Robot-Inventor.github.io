ddc06ac069f5f4c6c3c57ac1cf4206c01d8f1566
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-11-18 23:03:38 +0900

    Update basic.js

--- etc/js/basic.js
+++ etc/js/basic.js
@@ -1,12 +1,16 @@
-setTimeout($(".now_loading").fadeOut(2000), 3000);
+setTimeout(deleteProgress, 3000);
+
+var deleteProgress = function(){
+    $(".now_loading").fadeOut(2000);
+}
 
 $(function(){
     jQuery("header").load("/etc/html/header_menu.html");
     jQuery("footer").load("/etc/html/footer.min.html");
+    jQuery(".now_loading").fadeOut(2000);
     echo.init({
         throttle:"0"
     });
-    $(".now_loading").fadeOut(2000);
 });
 /*
 window.addEventListener('load', function() {
