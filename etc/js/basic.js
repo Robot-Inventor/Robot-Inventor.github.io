77e011e20f8c47fc7484178684cca56e73850882
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-11-18 23:25:14 +0900

    Update basic.js

--- etc/js/basic.js
+++ etc/js/basic.js
@@ -1,8 +1,8 @@
-setTimeout(deleteProgress, 3000);
-
 var deleteProgress = function(){
-    $(".now_loading").fadeOut(2000);
+    jQuery(".now_loading").fadeOut(2000);
 }
+
+setTimeout(deleteProgress, 3000);
 
 $(function(){
     jQuery("header").load("/etc/html/header_menu.html");
