7b71df9d9a7406c22066d0cfd9194f590e7e697f
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-11-18 22:21:42 +0900

    Update basic.js

--- etc/js/basic.js
+++ etc/js/basic.js
@@ -1,5 +1,5 @@
 $(function(){
-    jQuery("header").load("/etc/html/header_menu.min.html"),
+    jQuery("header").load("/etc/html/header_menu.html"),
     jQuery("footer").load("/etc/html/footer.min.html"),
     echo.init({
         throttle:"0"
