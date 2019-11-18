4264e339ec56d198574c72dfc8c4094e5445c372
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-11-19 00:14:23 +0900

    Update basic.js

--- etc/js/basic.js
+++ etc/js/basic.js
@@ -1,16 +1,7 @@
 jQuery(function(){
     jQuery("header").load("/etc/html/header_menu.html");
     jQuery("footer").load("/etc/html/footer.min.html");
-    clearTimeOut(deleteLoadingBarTimer);
-    deleteLoadingBar();
     echo.init({
         throttle:"0"
     });
 });
-
-
-var deleteLoadingBar = function(){
-    jQuery(".now_loading").fadeOut();
-}
-
-var deleteLoadingBarTimer = setTimeout(deleteLoadingBar, 3000);
