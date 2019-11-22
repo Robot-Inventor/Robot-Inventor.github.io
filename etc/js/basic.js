f6128e3f09de8d6e8e8767dfa8fba2bb1712ffdb
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-11-22 23:51:25 +0900

    Update basic.js

--- etc/js/basic.js
+++ etc/js/basic.js
@@ -1,6 +1,4 @@
 jQuery(function(){
-    var appendContent = '<script async src="/etc/js/echo.min.js"></script>' + '<script async src="/etc/js/read_notice.min.js"></script>' + '<script async src="/etc/js/load_new_article.min.js"></script>' + '<script async src="/etc/js/view_timer.min.js"></script>'
-    jQuery("body").append(appendContent);
     jQuery("header").load("/etc/html/header_menu.html");
     jQuery("footer").load("/etc/html/footer.min.html");
     echo.init({
