11dfb6c39558e74b9f628e2900dfafbd1fe229d6
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-11-29 16:01:45 +0900

    Update basic.js

--- etc/js/basic.js
+++ etc/js/basic.js
@@ -1,3 +1,10 @@
+var userAgent = window.navigator.userAgent.toLowerCase();
+
+if(userAgent.indexOf('msie') != -1 ||
+    userAgent.indexOf('trident') != -1) {
+        window.location.replace("/etc/html/do_not_use_ie.html");
+} 
+
 jQuery(function(){
     jQuery("header").load("/etc/html/header_menu.html");
     jQuery("footer").load("/etc/html/footer.min.html");
