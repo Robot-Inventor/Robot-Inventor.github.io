bca3a04ce7222ed9304329b3d8e72f6bf49798d3
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-12-01 00:37:12 +0900

    Update basic.js

--- etc/js/basic.js
+++ etc/js/basic.js
@@ -1,5 +1,4 @@
 var userAgent = window.navigator.userAgent.toLowerCase();
-
 if(userAgent.indexOf('msie') != -1 ||
     userAgent.indexOf('trident') != -1) {
         window.location.replace("/etc/html/do_not_use_ie.html");
