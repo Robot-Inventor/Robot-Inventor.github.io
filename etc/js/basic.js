ac6538f24b4809a053d241c9ff20c9d2ea06c1f5
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-12-01 12:12:38 +0900

    Update basic.js

--- etc/js/basic.js
+++ etc/js/basic.js
@@ -4,19 +4,18 @@ if(userAgent.indexOf('msie') != -1 ||
         window.location.replace("/etc/html/do_not_use_ie.html");
 }
 
-setTimeout(function(){
-    jQuery("footer").load("/etc/html/footer.html");
-},3000);
-setTimeout(function(){
-    if (document.cookie.indexOf("cookieConsent") == -1) {
-        alert("このサイトでは、皆さんに最高の体験をお届けするためにcookieを使用しています。詳しくはメニューよりプライバシーポリシーをご覧ください。");
-        document.cookie = 'cookieConsent=true"; max-age=2592000';
-    }
-},4000);
-
 jQuery(function(){
     jQuery("header").load("/etc/html/header_menu.html");
     echo.init({
         throttle:"0"
     });
+    setTimeout(function(){
+        jQuery("footer").load("/etc/html/footer.html");
+    },3000);
+    setTimeout(function(){
+        if (document.cookie.indexOf("cookieConsent") == -1) {
+            alert("このサイトでは、皆さんに最高の体験をお届けするためにcookieを使用しています。詳しくはメニューよりプライバシーポリシーをご覧ください。");
+            document.cookie = 'cookieConsent=true"; max-age=2592000';
+        }
+    },4000);
 });
