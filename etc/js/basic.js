13539e0633b18a8010e878cea378dd3575619fdc
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-12-04 23:34:28 +0900

    Update basic.js

--- etc/js/basic.js
+++ etc/js/basic.js
@@ -20,3 +20,13 @@ setTimeout(function(){
         document.cookie = 'cookieConsent=true"; max-age=2592000';
     }
 },5000);
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/service_worker.js').then(function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
