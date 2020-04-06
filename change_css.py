fd0e1fb128abc9693c15156977bee0570353e9de
Robot-Inventor <30.keita@gmail.com>
2020-04-06 19:24:13 +0900

    cssのプリロードを削除

--- change_css.py
+++ change_css.py
@@ -20,6 +20,7 @@ def change_css(html_file_path):
     with open(html_file_path, mode="r", newline="", encoding="utf-8_sig") as f:
         old_css = f.read()
         old_css = old_css.replace("\r\n", "\n").replace("\r", "\n")
+        old_css = old_css.replace('\n        <link rel="preload" href="/etc/css/main_style.min.css" as="style">', "")
     with open(html_file_path, mode="w", newline="", encoding="utf-8_sig") as f:
         pattern = re.compile(f"{CSS_START_MESSAGE}.*?{CSS_END_MESSAGE}", re.MULTILINE | re.DOTALL)
         old_css = re.sub(pattern, f"{CSS_START_MESSAGE}\n{template_css}\n{CSS_END_MESSAGE}", old_css)
