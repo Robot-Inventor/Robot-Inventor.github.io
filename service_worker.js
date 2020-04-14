7a6dfcff21aee2a3929f3aac783e5810246246f8
Robot-Inventor <30.keita@gmail.com>
2020-04-14 20:29:38 +0900

    service_worker.jsのキャッシュの内容を追加

--- service_worker.js
+++ service_worker.js
@@ -6,15 +6,39 @@ self.addEventListener('fetch', function(event) {
 
 workbox.precaching.precacheAndRoute([
     {
-        url: '/etc/js/basic.min.js',
-        revision: '9af3880'
-    },
-    {
         url: 'https://cdn.jsdelivr.net/npm/lazysizes@5.2.0/lazysizes.min.js',
-        revision: '0123456'
+        revision: '0'
     },
     {
         url: 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js',
-        revision: '0123456'
+        revision: '0'
+    },
+    {
+        url: '/icon/30.svg',
+        revision: '0'
+    },
+    {
+        url: '/icon/android-chrome-192x192.png',
+        revision: '0'
+    },
+    {
+        url: '/icon/favicon.ico',
+        revision: '0'
+    },
+    {
+        url: '/icon/site.webmanifest',
+        revision: '0'
+    },
+    {
+        url: 'https://apis.google.com/js/platform.js',
+        revision: '0'
+    },
+    {
+        url: 'https://www.google-analytics.com/analytics.js',
+        revision: '0'
+    },
+    {
+        url: 'https://platform.twitter.com/widgets.js',
+        revision: '0'
     }
 ])
