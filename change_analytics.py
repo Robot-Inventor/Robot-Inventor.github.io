a4d5fcddb2492b4c20d555479e6d645bb87636e4
Robot-Inventor <30.keita@gmail.com>
2020-04-14 15:19:11 +0900

    Google Analyticsの直書きのスクリプトにもdefer属性を追加

--- change_analytics.py
+++ change_analytics.py
@@ -13,8 +13,8 @@ def change_script(html_file_path):
         old_script = old_script.replace("\r\n", "\n").replace("\r", "\n")
     with open(html_file_path, mode="w", newline="", encoding="utf-8_sig") as f:
         old_script = old_script.replace(
-            '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-140830854-3"></script>',
-            '<script defer src="https://www.googletagmanager.com/gtag/js?id=UA-140830854-3"></script>')
+            '<script>\n            window.dataLayer = window.dataLayer || [];',
+            '<script defer>\n            window.dataLayer = window.dataLayer || [];')
         f.write(old_script)
 
 for file in glob.glob("**/*.html", recursive=True):
