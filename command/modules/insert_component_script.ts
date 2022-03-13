4f5e3e5e6e599766e8c0ad67abf360eed4bd44c8
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2022-03-14 02:37:10 +0900

    Note記法のCSSを整備

--- command/modules/insert_component_script.ts
+++ command/modules/insert_component_script.ts
@@ -5,9 +5,6 @@
 const insert_component_script = (document: Document) => {
     const component_table = {
         "yt-video": "/src/js/components/yt-video/yt-video.min.js",
-        "info-block": "/src/js/components/info-block/info-block.min.js",
-        "caution-block":
-            "/src/js/components/caution-block/caution-block.min.js",
         "article-card": "/src/js/components/article-card/article-card.min.js",
     } as const;
 
