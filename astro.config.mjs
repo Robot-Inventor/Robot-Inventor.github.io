449bffc8a3c10189e99462e04239ea77bbe48685
Robot-Inventor <30.keita@gmail.com>
2023-12-15 21:50:05 +0900

    コードブロックにexpressive-codeを採用

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { RemarkNotePlugin } from "@masatomakino/qiita-to-md/bin/plugin/RemarkNotePlugin";
+import expressiveCode from "astro-expressive-code";
 
+// https://astro.build/config
 export default defineConfig({
     site: "https://roboin.io",
-    integrations: [sitemap(), mdx()],
+    integrations: [
+        sitemap(),
+        expressiveCode({
+            themes: ["dark-plus", "light-plus"]
+        }),
+        mdx()
+    ],
     markdown: {
         shikiConfig: {
             theme: "dark-plus"
@@ -19,6 +27,6 @@ export default defineConfig({
     },
     redirects: {
         "/shadowban-scanner": "https://shadowban-scanner.roboin.io/",
-        "/shadowban-scanner/en": "https://shadowban-scanner.roboin.io/en/",
+        "/shadowban-scanner/en": "https://shadowban-scanner.roboin.io/en/"
     }
 });
