b0bf36bdb9145e3e07bac5e40c146900c5532f69
Robot-Inventor <30.keita@gmail.com>
2024-02-11 13:59:12 +0900

    remark-enhanced-link-cardのキャッシュを有効化

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,6 +35,7 @@ export default defineConfig({
             [
                 rlc,
                 {
+                    cache: true,
                     shortenUrl: true
                 }
             ]
