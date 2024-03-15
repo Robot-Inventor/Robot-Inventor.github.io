768ad7361c6a7b3b890cd038e188f256d739acfa
Robot-Inventor <30.keita@gmail.com>
2024-03-16 02:12:33 +0900

    型関連のエラーを修正

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -10,25 +10,27 @@ const getMimeType = (extension) => {
 };
 
 export const GET = async () => {
-    const posts = await getCollection('article');
+    const posts = await getCollection("article");
     return rss({
         title: `${import.meta.env.SITE_NAME} RSS Feed`,
         description: `${import.meta.env.SITE_NAME}のRSSフィードです。`,
         site: import.meta.env.SITE,
-        items: posts.sort((a, b) => (new Date(b.data.pubDate)).getTime() - (new Date(a.data.pubDate)).getTime()).map((post) => ({
-            title: post.data.title,
-            pubDate: post.data.pubDate,
-            description: post.data.description,
-            link: `/article/${post.slug}/`,
-            enclosure: {
-                url: post.data.thumbnail?.src || `${import.meta.env.SITE}/icon/ogp_default_thumbnail.png`,
-                type: post.data.thumbnail?.format ? getMimeType(post.data.thumbnail?.format) : "image/png",
-                length: 0
-            },
-        })),
+        items: posts
+            .sort((a, b) => new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime())
+            .map((post) => ({
+                title: post.data.title,
+                pubDate: new Date(post.data.pubDate),
+                description: post.data.description,
+                link: `/article/${post.slug}/`,
+                enclosure: {
+                    url: post.data.thumbnail?.src || `${import.meta.env.SITE}/icon/ogp_default_thumbnail.png`,
+                    type: post.data.thumbnail?.format ? getMimeType(post.data.thumbnail?.format) : "image/png",
+                    length: 0
+                }
+            })),
         customData: `<language>ja</language><webfeeds:icon>https://roboin.io/favicon.svg</webfeeds:icon>`,
         xmlns: {
             webfeeds: "http://webfeeds.org/rss/1.0"
         }
     });
-}
+};
