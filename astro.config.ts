3284a173ef1a857d6581d06a79daeb8229098c28
Robot-Inventor <30.keita@gmail.com>
2024-03-22 01:49:23 +0900

    CloudflareのSSRを有効化

--- astro.config.ts
+++ astro.config.ts
@@ -13,16 +13,14 @@ import darkModernTheme from "./src/themes/dark-modern.json";
 import lightModernTheme from "./src/themes/light-modern.json";
 import rehypeImageCaption from "rehype-image-caption";
 import remarkBreaks from "remark-breaks";
-
+import cloudflare from "@astrojs/cloudflare";
 const topPageURL = "https://roboin.io";
-
 pluginFramesTexts.overrideTexts("ja", {
-    copyButtonTooltip: "クリップボードにコピーする",
-    copyButtonCopied: "コピーしました！"
+  copyButtonTooltip: "クリップボードにコピーする",
+  copyButtonCopied: "コピーしました！"
 });
-
-const tocTemplate = (html) => {
-    return `
+const tocTemplate = html => {
+  return `
 <aside class="toc">
     <h2>目次</h2>
     <nav>
@@ -31,55 +29,41 @@ const tocTemplate = (html) => {
 </aside>`.trim();
 };
 
+
 // https://astro.build/config
 export default defineConfig({
-    site: topPageURL,
-    integrations: [
-        sitemap({
-            filter: (page) => !page.startsWith(new URL("/tag/", topPageURL).href)
-        }),
-        astroExpressiveCode({
-            themes: [darkModernTheme, lightModernTheme],
-            defaultLocale: "ja",
-            plugins: [pluginLineNumbers()],
-            defaultProps: {
-                overridesByLang: {
-                    "shell,sh,bash,powershell,console,shellsession,ansi": {
-                        showLineNumbers: false
-                    }
-                }
-            },
-            shiki: {
-                // @ts-expect-error
-                langs: [regexGrammar, shellSessionGrammar]
-            }
-        }),
-        customToc({
-            template: tocTemplate
-        }),
-        mdx()
-    ],
-    markdown: {
-        shikiConfig: {
-            theme: "dark-plus"
-        },
-        remarkPlugins: [
-            ...starlightAsides(),
-            [
-                rlc,
-                {
-                    cache: true,
-                    shortenUrl: true
-                }
-            ],
-            remarkBreaks
-        ],
-        rehypePlugins: [
-            [
-                rehypeAutoAds,
-                {
-                    countFrom: 2,
-                    adCode: `
+  site: topPageURL,
+  integrations: [sitemap({
+    filter: page => !page.startsWith(new URL("/tag/", topPageURL).href)
+  }), astroExpressiveCode({
+    themes: [darkModernTheme, lightModernTheme],
+    defaultLocale: "ja",
+    plugins: [pluginLineNumbers()],
+    defaultProps: {
+      overridesByLang: {
+        "shell,sh,bash,powershell,console,shellsession,ansi": {
+          showLineNumbers: false
+        }
+      }
+    },
+    shiki: {
+      // @ts-expect-error
+      langs: [regexGrammar, shellSessionGrammar]
+    }
+  }), customToc({
+    template: tocTemplate
+  }), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "dark-plus"
+    },
+    remarkPlugins: [...starlightAsides(), [rlc, {
+      cache: true,
+      shortenUrl: true
+    }], remarkBreaks],
+    rehypePlugins: [[rehypeAutoAds, ({
+      countFrom: 2,
+      adCode: `
 <ins class="adsbygoogle"
     style="display:block"
     data-ad-client="ca-pub-2526648882773973"
@@ -89,13 +73,12 @@ export default defineConfig({
 <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
 </script>`.trim()
-                } satisfies Parameters<typeof rehypeAutoAds>[0]
-            ],
-            rehypeImageCaption
-        ]
-    },
-    redirects: {
-        "/shadowban-scanner": "https://shadowban-scanner.roboin.io/",
-        "/shadowban-scanner/en": "https://shadowban-scanner.roboin.io/en/"
-    }
+    } satisfies Parameters<typeof rehypeAutoAds>[0])], rehypeImageCaption]
+  },
+  redirects: {
+    "/shadowban-scanner": "https://shadowban-scanner.roboin.io/",
+    "/shadowban-scanner/en": "https://shadowban-scanner.roboin.io/en/"
+  },
+  output: "server",
+  adapter: cloudflare()
 });
