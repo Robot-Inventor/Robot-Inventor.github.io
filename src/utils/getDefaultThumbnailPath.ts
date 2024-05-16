c02cdbfe0108d0269faab4d582f0bf15145009ca
Robot-Inventor <30.keita@gmail.com>
2024-05-16 10:56:45 +0900

    広告を追加

--- src/utils/getDefaultThumbnailPath.ts
+++ src/utils/getDefaultThumbnailPath.ts
@@ -1,4 +1,8 @@
-const getDefaultThumbnail = (slug: string): ImageMetadata => {
+import type { CollectionEntry } from "astro:content";
+
+const getDefaultThumbnail = (
+    slug: CollectionEntry<"article">["slug"] | `/article/${CollectionEntry<"article">["slug"]}/`
+): ImageMetadata => {
     const defaultThumbnail: ImageMetadata = {
         width: 1280,
         height: 720,
