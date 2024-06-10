8eac02a9c774e99365a7e198e4202d3880a75410
Robot-Inventor <30.keita@gmail.com>
2024-06-10 13:38:06 +0900

    エラーが出るので一部のコードを`process.env`に戻す
    `Only URLs with a scheme in: file, data, and node are supported by the default ESM loader. Received protocol 'astro:'`

--- src/utils/checkEnvironmentType.ts
+++ src/utils/checkEnvironmentType.ts
@@ -1,11 +1,9 @@
-import { getSecret } from "astro:env/server";
-
 const checkEnvironmentType = () => {
     if (!process.env.CF_PAGES_BRANCH) {
         return "local";
     }
 
-    return ["master", "main"].includes(getSecret("CF_PAGES_BRANCH")) ? "production" : "test";
+    return ["master", "main"].includes(process.env.CF_PAGES_BRANCH) ? "production" : "test";
 };
 
 export { checkEnvironmentType };
