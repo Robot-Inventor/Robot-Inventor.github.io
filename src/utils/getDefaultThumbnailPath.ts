ba80f1613a44d2403bac5022f360acda99ee4907
Robot-Inventor <30.keita@gmail.com>
2024-06-10 15:37:57 +0900

    一部のページでOGP画像が正しく設定されないバグを修正

--- src/utils/getDefaultThumbnailPath.ts
+++ src/utils/getDefaultThumbnailPath.ts
@@ -1,19 +1,28 @@
-import type { CollectionEntry } from "astro:content";
+import { getEntry } from "astro:content";
 
-const getDefaultThumbnail = (
-    slug: CollectionEntry<"article">["slug"] | `/article/${CollectionEntry<"article">["slug"]}/`
-): ImageMetadata => {
-    const defaultThumbnail: ImageMetadata = {
-        width: 1280,
-        height: 720,
-        format: "png",
-        src: `/og/${slug
-            .replace(/^\//, "")
-            .replace(/^article\//, "")
-            .replace(/\/$/, "")}.png`
-    } as const;
+const getDefaultThumbnail = async (slug: string): Promise<ImageMetadata> => {
+    const cleanedSlug = slug
+        .replace(/^\//, "")
+        .replace(/^article\//, "")
+        .replace(/\/$/, "");
 
-    return defaultThumbnail;
+    const entry = cleanedSlug ? await getEntry("article", cleanedSlug) : null;
+
+    if (entry) {
+        return {
+            width: 1280,
+            height: 720,
+            format: "png",
+            src: `/og/${cleanedSlug}.png`
+        };
+    } else {
+        return {
+            width: 1920,
+            height: 960,
+            format: "png",
+            src: "/icon/ogp_default_thumbnail.png"
+        };
+    }
 };
 
 export { getDefaultThumbnail };
