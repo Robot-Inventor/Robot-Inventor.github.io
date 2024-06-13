0a4e6ecce81305f0b5c7398ec77ee8cb48bc1c5e
Robot-Inventor <30.keita@gmail.com>
2024-06-13 15:51:11 +0900

    コードの構造を変更

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,24 @@
-export async function onRequest(context) {
-    const { request, env } = context;
-    const response = await env.ASSETS.fetch(request);
+// export async function onRequest(context) {
+//     const { request, env } = context;
+//     const response = await env.ASSETS.fetch(request);
+//     return new HTMLRewriter().on("head", new ElementHandler()).transform(response);
+// }
+
+async function handleRequest(request) {
+    const response = await fetch(request);
     return new HTMLRewriter().on("head", new ElementHandler()).transform(response);
 }
 
+addEventListener("fetch", async event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+
 class ElementHandler {
+    element(element) {
+        console.log("element found: ", element.tagName);
+    }
+
     comments(comment) {
         console.log(`comment found: "${comment.text}"`);
         if (comment.text.trim() !== "HEAD_AD_SCRIPT") return;
