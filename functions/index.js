f06f1c940be454b49615e79c02687e47a29723d4
Robot-Inventor <30.keita@gmail.com>
2024-06-13 15:36:07 +0900

    fix typo

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ export async function onRequest(context) {
 
 class ElementHandler {
     comments(comment) {
-        if (comment.text.strip() !== "HEAD_AD_SCRIPT") return;
+        if (comment.text.trim() !== "HEAD_AD_SCRIPT") return;
 
         comment.replace('<script is:inline async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js" />', { html: true });
     }
