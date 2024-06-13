8623e2e6570617c7f76324f64e45b4641513b320
Robot-Inventor <30.keita@gmail.com>
2024-06-13 17:15:10 +0900

    MicroAd COMPASSを導入

--- functions/[[index]].js
+++ functions/[[index]].js
@@ -1,4 +1,4 @@
-const AD_SCRIPTS = {
+const I_MOBILE_AD_SCRIPTS = {
     HEAD_AD_SCRIPT: `
 <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
 <script>
@@ -51,18 +51,141 @@ const AD_SCRIPTS = {
 `.trim()
 };
 
-export async function onRequest(context) {
+const MICROAD_AD_SCRIPTS = {
+    HEAD_AD_SCRIPT: ``,
+    BODY_AD_SCRIPT: `
+<script type="text/javascript">
+var microadCompass = microadCompass || {};
+microadCompass.queue = microadCompass.queue || [];
+</script>
+<script type="text/javascript" charset="UTF-8" src="//j.microad.net/js/compass.js" onload="new microadCompass.AdInitializer().initialize();" async></script>
+<div id="9f7b2995e6ba1c4c3951dfc6c41ade36" style="visibility: hidden; width: 0px; height: 0px; display: none;">
+        <script type="text/javascript">
+                microadCompass.queue.push({
+                        "spot": "9f7b2995e6ba1c4c3951dfc6c41ade36"
+                });
+        </script>
+</div>
+<div id="ads-ADU-8CD0H0NJ"></div>
+<script>
+  (function (w, d, s, i) {
+    var CREATIVE_ID = "9f7b2995e6ba1c4c3951dfc6c41ade36";
+    var microadTag, countNumber = 0, isFindingMicroAdBanner = false;
+
+    var findCreativeTagInterval = setInterval(findCreativeTag, 50);
+    function findCreativeTag() {
+      if (countNumber > 50) {
+        clearInterval(findCreativeTagInterval);
+        // add 2 following lines for Passback
+        loadAdunit(microadTag.id);
+        return;
+      }
+      if (isFindingMicroAdBanner) return;
+      isFindingMicroAdBanner = true;
+      var divTags = document.getElementsByTagName('div');
+      countNumber = countNumber + 1;
+      if (divTags && divTags.length) {
+        for (var i = 0; i < divTags.length; i++) {
+          var divTag = divTags[i];
+          if (divTag && divTag.id && divTag.id.indexOf(CREATIVE_ID) > -1) {
+            microadTag = divTag;
+            microadTag.style.visibility = "hidden";
+            if (microadTag && microadTag.getElementsByTagName('iframe') &&
+              microadTag.getElementsByTagName('iframe').length > 0) {
+              loadAdunit(microadTag.id);
+              clearInterval(findCreativeTagInterval);
+              break;
+            }
+          }
+        }
+      }
+      isFindingMicroAdBanner = false;
+    }
+    function loadAdunit(microadBannerId) {
+      w.adunitlength ? w.adunitlength.push('ADU-8CD0H0NJ') : w.adunitlength=['ADU-8CD0H0NJ']; var f=d.getElementsByTagName(s)[0], j=d.createElement(s);
+      j.async=true; j.src='https://rise.enhance.co.jp/adu.js?id=' + i + '&matwName=matw_uuid' + '&clientTime=' + (new Date()).getTime() + '&maBannerId=' + microadBannerId;
+      f.parentNode.insertBefore(j, f);
+    }
+  })(window, document, 'script', 'ADU-8CD0H0NJ');
+</script>
+<script type="text/javascript">
+var microadCompass = microadCompass || {};
+microadCompass.queue = microadCompass.queue || [];
+</script>
+<script type="text/javascript" charset="UTF-8" src="//j.microad.net/js/compass.js" onload="new microadCompass.AdInitializer().initialize();" async></script>
+<div id="65e713adcd923407d77bed5066957aca" style="visibility: hidden; width: 0px; height: 0px; display: none;">
+        <script type="text/javascript">
+                microadCompass.queue.push({
+                        "spot": "65e713adcd923407d77bed5066957aca"
+                });
+        </script>
+</div>
+<div id="ads-ADU-BSBU4LKR"></div>
+<script>
+  (function (w, d, s, i) {
+    var CREATIVE_ID = "65e713adcd923407d77bed5066957aca";
+    var microadTag, countNumber = 0, isFindingMicroAdBanner = false;
+
+    var findCreativeTagInterval = setInterval(findCreativeTag, 50);
+    function findCreativeTag() {
+      if (countNumber > 50) {
+        clearInterval(findCreativeTagInterval);
+        // add 2 following lines for Passback
+        loadAdunit(microadTag.id);
+        return;
+      }
+      if (isFindingMicroAdBanner) return;
+      isFindingMicroAdBanner = true;
+      var divTags = document.getElementsByTagName('div');
+      countNumber = countNumber + 1;
+      if (divTags && divTags.length) {
+        for (var i = 0; i < divTags.length; i++) {
+          var divTag = divTags[i];
+          if (divTag && divTag.id && divTag.id.indexOf(CREATIVE_ID) > -1) {
+            microadTag = divTag;
+            microadTag.style.visibility = "hidden";
+            if (microadTag && microadTag.getElementsByTagName('iframe') &&
+              microadTag.getElementsByTagName('iframe').length > 0) {
+              loadAdunit(microadTag.id);
+              clearInterval(findCreativeTagInterval);
+              break;
+            }
+          }
+        }
+      }
+      isFindingMicroAdBanner = false;
+    }
+    function loadAdunit(microadBannerId) {
+      w.adunitlength ? w.adunitlength.push('ADU-BSBU4LKR') : w.adunitlength=['ADU-BSBU4LKR']; var f=d.getElementsByTagName(s)[0], j=d.createElement(s);
+      j.async=true; j.src='https://rise.enhance.co.jp/adu.js?id=' + i + '&matwName=matw_uuid' + '&clientTime=' + (new Date()).getTime() + '&maBannerId=' + microadBannerId;
+      f.parentNode.insertBefore(j, f);
+    }
+  })(window, document, 'script', 'ADU-BSBU4LKR');
+</script>
+`.trim()
+};
+
+const selectRandomArray = (array) => array[Math.floor(Math.random() * array.length)];
+
+export const onRequest = async (context) => {
     const { request, env } = context;
     const response = await env.ASSETS.fetch(request);
-    return new HTMLRewriter().on("head, body", new ElementHandler()).transform(response);
+
+    const adScripts = selectRandomArray([I_MOBILE_AD_SCRIPTS, MICROAD_AD_SCRIPTS]);
+
+    return new HTMLRewriter().on("head, body", new ElementHandler(adScripts)).transform(response);
 }
 
 class ElementHandler {
+    constructor(adScripts) {
+        this.adScripts = adScripts;
+    }
+
     comments(comment) {
         const commentString = comment.text.trim();
 
-        if (!(commentString in AD_SCRIPTS)) return;
+        if (!(commentString in this.adScripts)) return;
 
-        comment.replace(AD_SCRIPTS[commentString], { html: true });
+        comment.replace(this.adScripts[commentString], { html: true });
     }
 }
