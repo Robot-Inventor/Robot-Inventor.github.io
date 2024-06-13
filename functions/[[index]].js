cf6075709525a80b429e9e4f52078ce49e04b5f0
Robot-Inventor <30.keita@gmail.com>
2024-06-13 16:55:52 +0900

    不要な`is:inline`属性を削除

--- functions/[[index]].js
+++ functions/[[index]].js
@@ -1,7 +1,7 @@
 const AD_SCRIPTS = {
     HEAD_AD_SCRIPT: `
 <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
-<script is:inline>
+<script>
     window.googletag = window.googletag || { cmd: [] };
     googletag.cmd.push(function () {
         googletag
@@ -29,7 +29,7 @@ const AD_SCRIPTS = {
     style="position:fixed;bottom:0;padding-bottom:env(safe-area-inset-bottom);left:0;right:0;width:100%;background:rgba(0, 0, 0, 0);z-index:99998;text-align:center;transform:translate3d(0, 0, 0);"
 >
     <div id="div-gpt-ad-1713946891063-0" style="margin:auto;text-align:center;z-index:99999;">
-        <script is:inline>
+        <script>
             googletag.cmd.push(function () {
                 googletag.display("div-gpt-ad-1713946891063-0");
             });
@@ -41,7 +41,7 @@ const AD_SCRIPTS = {
     style="position:fixed;bottom:0;padding-bottom:env(safe-area-inset-bottom);left:0;right:0;width:100%;background:rgba(0, 0, 0, 0);z-index:99998;text-align:center;transform:translate3d(0, 0, 0);"
 >
     <div id="div-gpt-ad-1713946948591-0" style="margin:auto;text-align:center;z-index:99999;">
-        <script is:inline>
+        <script>
             googletag.cmd.push(function () {
                 googletag.display("div-gpt-ad-1713946948591-0");
             });
