2d015ce9e59446f07711f70f020232a0cac41b36
Robot-Inventor <30.keita@gmail.com>
2024-06-13 15:27:58 +0900

    広告の処理をCloudflare Workersで実装

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+export async function onRequest(context) {
+    const { request, env } = context;
+    const response = await env.ASSETS.fetch(request);
+    return new HTMLRewriter().on("head", new ElementHandler()).transform(response);
+}
+
+class ElementHandler {
+    comments(comment) {
+        if (comment.text.strip() !== "HEAD_AD_SCRIPT") return;
+
+        comment.replace('<script is:inline async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js" />', { html: true });
+    }
+}
