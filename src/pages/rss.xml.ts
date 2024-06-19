52734b67c346245e4dbe5c8dabaf588db0368d89
Robot-Inventor <30.keita@gmail.com>
2024-06-19 14:35:10 +0900

    RSSフィードのサムネイルがundefinedになることがあるバグを修正

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -9,30 +9,34 @@ const getMimeType = (extension) => {
 
 export const GET = async () => {
     const posts = await getCollection("article");
+    const sortedPosts = posts.sort((a, b) => new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime());
+
+    const rssEntries = await Promise.all(
+        sortedPosts.map(async (post) => {
+            const defaultThumbnail = await getDefaultThumbnail(post.slug);
+            return {
+                title: post.data.title,
+                pubDate: new Date(post.data.pubDate),
+                description: post.data.description,
+                link: `/article/${post.slug}/`,
+                enclosure: {
+                    url: post.data.thumbnail
+                        ? post.data.thumbnail.src
+                        : new URL(defaultThumbnail.src, import.meta.env.SITE).href,
+                    type: post.data.thumbnail
+                        ? getMimeType(post.data.thumbnail.format)
+                        : getMimeType(defaultThumbnail.format),
+                    length: 0
+                }
+            };
+        })
+    );
+
     return rss({
         title: `${import.meta.env.SITE_NAME} RSS Feed`,
         description: `${import.meta.env.SITE_NAME}のRSSフィードです。`,
         site: import.meta.env.SITE,
-        items: posts
-            .sort((a, b) => new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime())
-            .map((post) => {
-                const defaultThumbnail = getDefaultThumbnail(post.slug);
-                return {
-                    title: post.data.title,
-                    pubDate: new Date(post.data.pubDate),
-                    description: post.data.description,
-                    link: `/article/${post.slug}/`,
-                    enclosure: {
-                        url: post.data.thumbnail
-                            ? post.data.thumbnail.src
-                            : new URL(defaultThumbnail.src, import.meta.env.SITE).href,
-                        type: post.data.thumbnail
-                            ? getMimeType(post.data.thumbnail.format)
-                            : getMimeType(defaultThumbnail.format),
-                        length: 0
-                    }
-                };
-            }),
+        items: rssEntries,
         customData: `<language>ja</language><webfeeds:icon>https://roboin.io/favicon.svg</webfeeds:icon>`,
         xmlns: {
             webfeeds: "http://webfeeds.org/rss/1.0"
