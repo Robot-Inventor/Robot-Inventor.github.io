29fe74e7449170935f85cbecbb7f92f9256b0043
Robot-Inventor <30.keita@gmail.com>
2024-06-19 14:43:52 +0900

    注目記事関連のコードのリファクタリング

--- src/utils/getFeaturedArticles.ts
+++ src/utils/getFeaturedArticles.ts
@@ -1,23 +1,26 @@
 import { getCollection, getEntry } from "astro:content";
+import type { CollectionEntry } from "astro:content";
 import { getDefaultThumbnail } from "./getDefaultThumbnailPath";
 
 const getFeaturedArticles = async () => {
     const featuredArticles = await getCollection("featuredArticles");
     if (featuredArticles.length === 0) return null;
 
+    const flattenFeaturedArticles = featuredArticles
+        .map((data) => data.data.articles as Array<CollectionEntry<"article">>)
+        .flat();
+
     const result: { title: string; link: string; thumbnail: ImageMetadata }[] = [];
 
-    for (const featuredArticleData of featuredArticles) {
-        for (const article of featuredArticleData.data.articles) {
-            if (result.filter((data) => data.link === `/article/${article.slug}/`).length) continue;
+    for (const article of flattenFeaturedArticles) {
+        if (result.filter((data) => data.link === `/article/${article.slug}/`).length) continue;
 
-            const collectionData = await getEntry("article", article.slug);
-            result.push({
-                title: collectionData.data.title,
-                link: `/article/${article.slug}/`,
-                thumbnail: collectionData.data.thumbnail || getDefaultThumbnail(article.slug)
-            });
-        }
+        const collectionData = await getEntry("article", article.slug);
+        result.push({
+            title: collectionData.data.title,
+            link: `/article/${article.slug}/`,
+            thumbnail: collectionData.data.thumbnail || (await getDefaultThumbnail(article.slug))
+        });
     }
 
     return result;
