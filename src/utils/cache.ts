bfb34e8765d88979dc7b5cbd3d6d7138db218cbc
Robot-Inventor <30.keita@gmail.com>
2024-07-29 19:35:32 +0900

    不要なファイルを削除

--- src/utils/cache.ts
+++
@@ -1,9 +0,0 @@
-const cacheMap = new Map<string, any>();
-
-const cache = (key: string, factory: () => any) => {
-    if (!cacheMap.has(key)) {
-        cacheMap.set(key, factory());
-    }
-
-    return cacheMap.get(key);
-};
