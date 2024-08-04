1a242e71d4b9dd609dc17d3246d8a3fde0cb767b
Robot-Inventor <30.keita@gmail.com>
2024-08-05 04:25:05 +0900

    人気記事にトレンドアルゴリズムを実装

--- scripts/updatePopularArticles.ts
+++ scripts/updatePopularArticles.ts
@@ -1,27 +1,32 @@
 import { BetaAnalyticsDataClient } from "@google-analytics/data";
+import type { google } from "@google-analytics/data/build/protos/protos";
 import fs from "fs";
-import { checkEnvironmentType } from "src/utils/checkEnvironmentType";
+import { checkEnvironmentType } from "../src/utils/checkEnvironmentType";
 
-async function getPopularArticles() {
-    const propertyId = "309465986";
-    const analyticsDataClient = new BetaAnalyticsDataClient({
-        // @ts-expect-error
-        credentials: JSON.parse(process.env.GA_CREDENTIALS)
-    });
+const getPreviousDate = (date: Date): Date => {
+    const previousDate = new Date(date);
+    previousDate.setDate(previousDate.getDate() - 1);
+    return previousDate;
+};
 
-    const currentDate = new Date();
-    currentDate.setHours(currentDate.getHours() + 9);
-    const currentHour = currentDate.getHours();
-    const startDate = currentHour < 12 ? getPreviousDate(currentDate) : getCurrentDate(currentDate);
+const getCurrentDate = (date: Date): Date => {
+    return new Date(date);
+};
 
+const normalize = (values: number[]) => {
+    const max = Math.max(...values);
+    const min = Math.min(...values);
+    return values.map((value) => (value - min) / (max - min));
+};
+
+const getReport = async (
+    analyticsDataClient: BetaAnalyticsDataClient,
+    propertyId: string,
+    dateRange: google.analytics.data.v1beta.IDateRange
+): Promise<google.analytics.data.v1beta.IRunReportResponse> => {
     const [response] = await analyticsDataClient.runReport({
         property: `properties/${propertyId}`,
-        dateRanges: [
-            {
-                startDate: startDate.toISOString().split("T")[0],
-                endDate: "today"
-            }
-        ],
+        dateRanges: [dateRange],
         dimensions: [
             {
                 name: "pagePath"
@@ -29,39 +34,173 @@ async function getPopularArticles() {
         ],
         metrics: [
             {
-                name: "activeUsers"
+                name: "screenPageViews"
             }
         ],
         orderBys: [
             {
                 metric: {
-                    metricName: "activeUsers"
+                    metricName: "screenPageViews"
                 },
                 desc: true
             }
         ],
-        limit: 5
+        limit: 300
     });
 
-    function getPreviousDate(date: Date): Date {
-        const previousDate = new Date(date);
-        previousDate.setDate(previousDate.getDate() - 1);
-        return previousDate;
+    return response;
+};
+
+const compareReports = (
+    previousReport: google.analytics.data.v1beta.IRunReportResponse,
+    latestReport: google.analytics.data.v1beta.IRunReportResponse,
+    latestReportWeight: number
+): string[] => {
+    const previousRows = previousReport.rows || [];
+    const latestRows = latestReport.rows || [];
+
+    const previousData: { [key: string]: number } = {};
+    const latestData: { [key: string]: number } = {};
+
+    for (const row of previousRows) {
+        if (!row.dimensionValues || !row.metricValues) continue;
+
+        const path = row.dimensionValues[0].value;
+        if (typeof path !== "string" || !path.startsWith("/article/")) continue;
+
+        const cleanedSlug = path.replace(/^\/article\//, "").replace(/\/$/, "");
+        // 記事一覧ページを除外
+        if (!cleanedSlug) continue;
+        const views = parseInt(row.metricValues[0].value || "0", 10);
+        previousData[cleanedSlug] = views;
     }
 
-    function getCurrentDate(date: Date): Date {
-        return new Date(date);
+    for (const row of latestRows) {
+        if (!row.dimensionValues || !row.metricValues) continue;
+
+        const path = row.dimensionValues[0].value;
+        if (typeof path !== "string" || !path.startsWith("/article/")) continue;
+
+        const cleanedSlug = path.replace(/^\/article\//, "").replace(/\/$/, "");
+        // 記事一覧ページを除外
+        if (!cleanedSlug) continue;
+        const views = parseInt(row.metricValues[0].value || "0", 10) * latestReportWeight;
+        latestData[cleanedSlug] = views;
     }
 
-    if (!response.rows) {
-        throw new Error("No analytics data found.");
+    const scoredArticles: { path: string; score: number }[] = [];
+    const increases: number[] = [];
+    const increaseRates: number[] = [];
+    const previousViewsArray: number[] = [];
+    const latestViewsArray: number[] = [];
+
+    for (const path in latestData) {
+        const latestViews = latestData[path];
+        const previousViews = previousData[path] || 0;
+        const increase = latestViews - previousViews;
+        const increaseRate = previousViews > 0 ? (increase / previousViews) * 100 : 100;
+
+        increases.push(increase);
+        increaseRates.push(increaseRate);
+        previousViewsArray.push(previousViews);
+        latestViewsArray.push(latestViews);
     }
 
-    return response.rows.map((row) => {
+    const normalizedIncreases = normalize(increases);
+    const normalizedIncreaseRates = normalize(increaseRates);
+    const normalizedPreviousViews = normalize(previousViewsArray);
+    const normalizedLatestViews = normalize(latestViewsArray);
+
+    let index = 0;
+    for (const path in latestData) {
+        const normalizedIncrease = normalizedIncreases[index];
+        const normalizedIncreaseRate = normalizedIncreaseRates[index];
+        const normalizedPreviousView = normalizedPreviousViews[index];
+        const normalizedLatestView = normalizedLatestViews[index];
+        index++;
+
+        let weightIncrease = 0.1;
+        let weightRate = 0.5;
+        let weightPreviousView = 0.3;
+        let weightLatestView = 0.1;
+
+        if (normalizedPreviousView < 0.25) {
+            weightIncrease = 0.5;
+            weightRate = 0.1;
+            weightPreviousView = 0.1;
+            weightLatestView = 0.3;
+        } else if (normalizedPreviousView < 0.75) {
+            weightIncrease = 0.3;
+            weightRate = 0.3;
+            weightPreviousView = 0.2;
+            weightLatestView = 0.2;
+        } else {
+            weightIncrease = 0.1;
+            weightRate = 0.5;
+            weightPreviousView = 0.3;
+            weightLatestView = 0.1;
+        }
+
+        const score =
+            normalizedIncrease * weightIncrease +
+            normalizedIncreaseRate * weightRate +
+            normalizedPreviousView * weightPreviousView +
+            normalizedLatestView * weightLatestView;
+
+        scoredArticles.push({ path, score });
+    }
+
+    scoredArticles.sort((a, b) => b.score - a.score);
+
+    const trendingArticles = scoredArticles.slice(0, 5).map((article) => article.path);
+
+    return trendingArticles;
+};
+
+const getTrendingArticles = async () => {
+    const propertyId = "309465986";
+    const analyticsDataClient = new BetaAnalyticsDataClient({
         // @ts-expect-error
-        return row.dimensionValues[0].value?.replace(/^\/article\//, "").replace(/\/$/, "");
+        credentials: JSON.parse(process.env.GA_CREDENTIALS)
     });
-}
+
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+
+    // 日本時間で15時より前なら前日のデータ、15時以降なら当日のデータを取得する
+    const thresholdHour = 15;
+    const timeZoneOffset = 9;
+
+    const startDateLatest =
+        currentHour + timeZoneOffset < thresholdHour ? getPreviousDate(currentDate) : getCurrentDate(currentDate);
+
+    const latestReportWeight =
+        currentHour + timeZoneOffset < thresholdHour
+            ? 1
+            : 2 - (currentHour + timeZoneOffset - thresholdHour) / (24 - thresholdHour);
+
+    const startDatePrevious = getPreviousDate(startDateLatest);
+
+    const dateRanges: google.analytics.data.v1beta.IDateRange[] = [
+        {
+            name: "last24Hours",
+            startDate: startDateLatest.toISOString().split("T")[0],
+            endDate: startDateLatest.toISOString().split("T")[0]
+        },
+        {
+            name: "previous24Hours",
+            startDate: startDatePrevious.toISOString().split("T")[0],
+            endDate: startDatePrevious.toISOString().split("T")[0]
+        }
+    ] as const;
+
+    const latestPeriodReport = await getReport(analyticsDataClient, propertyId, dateRanges[0]);
+    const previousPeriodReport = await getReport(analyticsDataClient, propertyId, dateRanges[1]);
+
+    const trendingArticles = compareReports(previousPeriodReport, latestPeriodReport, latestReportWeight);
+
+    return trendingArticles;
+};
 
 const main = async () => {
     const environmentType = checkEnvironmentType();
@@ -77,7 +216,7 @@ const main = async () => {
         return;
     }
 
-    const popularArticles = await getPopularArticles();
+    const popularArticles = await getTrendingArticles();
     const jsonData = {
         articles: popularArticles
     };
